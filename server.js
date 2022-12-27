2e8e15a970ddf565185e6286c0fc0dfbaa1208ed
utkarsh1996-3 <119508950+utkarsh1996-3@users.noreply.github.com>
2022-12-27 13:07:42 +0530

    solution

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
